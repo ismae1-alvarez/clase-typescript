69890da542a5b0b61bd0a83495046122b01aa058
Ismael Alvarez <160186634+ismae1-alvarez@users.noreply.github.com>
2025-03-24 19:36:17 -0600

    Delete dist directory

--- dist/arrayTuplas.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.result = exports.usersAge = exports.users = void 0;
-const users = ["Juan", "Pepito", "Anna"];
-exports.users = users;
-let usersAge = ["Jairo", 23, 23423];
-exports.usersAge = usersAge;
-let result = [true, "Resultado"];
-exports.result = result;
